aeeaf68142e5e0c7f1b65ebc2a3b3e04d93a3f9d
Giulia Jairo <jairogiulia93@gmail.com>
2025-06-02 13:02:47 -0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace DiaDosNamorados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPergunta = new System.Windows.Forms.Label();
+            this.btnSim = new System.Windows.Forms.Button();
+            this.btnNao = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.llbMusica = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblPergunta
+            // 
+            this.lblPergunta.AutoSize = true;
+            this.lblPergunta.Font = new System.Drawing.Font("SimSun", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPergunta.Location = new System.Drawing.Point(61, 200);
+            this.lblPergunta.Name = "lblPergunta";
+            this.lblPergunta.Size = new System.Drawing.Size(566, 35);
+            this.lblPergunta.TabIndex = 0;
+            this.lblPergunta.Text = "Quer ser meu par para sempre?\r\n";
+            // 
+            // btnSim
+            // 
+            this.btnSim.Font = new System.Drawing.Font("MV Boli", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSim.Location = new System.Drawing.Point(236, 268);
+            this.btnSim.Name = "btnSim";
+            this.btnSim.Size = new System.Drawing.Size(87, 35);
+            this.btnSim.TabIndex = 1;
+            this.btnSim.Text = "Sim";
+            this.btnSim.UseVisualStyleBackColor = true;
+            this.btnSim.Click += new System.EventHandler(this.btnSim_Click);
+            // 
+            // btnNao
+            // 
+            this.btnNao.Font = new System.Drawing.Font("MV Boli", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNao.Location = new System.Drawing.Point(343, 268);
+            this.btnNao.Name = "btnNao";
+            this.btnNao.Size = new System.Drawing.Size(81, 35);
+            this.btnNao.TabIndex = 2;
+            this.btnNao.Text = "Não";
+            this.btnNao.UseVisualStyleBackColor = true;
+            this.btnNao.Click += new System.EventHandler(this.btnNao_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DiaDosNamorados.Properties.Resources.teddy_bear_1_;
+            this.pictureBox1.Location = new System.Drawing.Point(236, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(206, 148);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // llbMusica
+            // 
+            this.llbMusica.AutoSize = true;
+            this.llbMusica.LinkColor = System.Drawing.Color.Indigo;
+            this.llbMusica.Location = new System.Drawing.Point(312, 401);
+            this.llbMusica.Name = "llbMusica";
+            this.llbMusica.Size = new System.Drawing.Size(53, 13);
+            this.llbMusica.TabIndex = 4;
+            this.llbMusica.TabStop = true;
+            this.llbMusica.Text = "Clica aqui";
+            this.llbMusica.VisitedLinkColor = System.Drawing.Color.Green;
+            this.llbMusica.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.llbMusica_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Thistle;
+            this.ClientSize = new System.Drawing.Size(682, 451);
+            this.Controls.Add(this.llbMusica);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnNao);
+            this.Controls.Add(this.btnSim);
+            this.Controls.Add(this.lblPergunta);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dia dos namorados ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPergunta;
+        private System.Windows.Forms.Button btnSim;
+        private System.Windows.Forms.Button btnNao;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.LinkLabel llbMusica;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiaDosNamorados
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int distancia = 100; // distância mínima pra ele fugir
+
+            int dx = e.X - btnNao.Location.X;
+            int dy = e.Y - btnNao.Location.Y;
+
+            if (Math.Abs(dx) < distancia && Math.Abs(dy) < distancia)
+            {
+                Random aleatorio = new Random();
+                int novaX = aleatorio.Next(this.ClientSize.Width - btnNao.Width);
+                int novaY = aleatorio.Next(this.ClientSize.Height - btnNao.Height);
+
+                btnNao.Location = new Point(novaX, novaY);
+
+            }
+        }
+
+        private void btnSim_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+    "Eu sabia que era sim, te amo pra sempre.💜 \nFeliz Dia dos Namorados!","hihihihi", MessageBoxButtons.OK,MessageBoxIcon.Information);
+
+        }
+
+        private void llbMusica_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=cxkSy7drF_U");
+        }
+
+        private void btnNao_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Como assim?? \ntudo bem então... sai do meu app!",
+                "Ei!",  MessageBoxButtons.OK, MessageBoxIcon.Error);
+            Application.Exit();
+        }
+    }
+}
